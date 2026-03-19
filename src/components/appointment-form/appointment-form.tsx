cf62a965555b25deaabae7cabffe3c2041794181
pietr <pietrokucharski@hotmail.com>
2026-03-19 10:21:32 -0300

    feat: add initial form inputs for appointment form

--- src/components/appointment-form/appointment-form.tsx
+++ src/components/appointment-form/appointment-form.tsx
@@ -12,14 +12,16 @@ import {
 import { z } from 'zod';
 import { Controller, useForm } from 'react-hook-form';
 import { zodResolver } from '@hookform/resolvers/zod';
-import { Field, FieldGroup, FieldLabel } from '../ui/field';
+import { Field, FieldError, FieldGroup, FieldLabel } from '../ui/field';
 import { Input } from '../ui/input';
+import { Dog, User } from 'lucide-react';
+import { Textarea } from '../ui/textarea';
 
 const appointmentsFormSchema = z.object({
   tutorName: z.string().min(3, 'O nome do tutor é obrigatório'),
-  // petName: z.string().min(3, 'O nome do pet é obrigatório'),
-  // phone: z.string().min(11, 'O telefone é obrigatório'),
-  // description: z.string().min(3, 'A descrição é obrigatória'),
+  petName: z.string().min(3, 'O nome do pet é obrigatório'),
+  phone: z.string().min(11, 'O telefone é obrigatório'),
+  description: z.string().min(3, 'A descrição é obrigatória'),
 });
 
 type AppointmentsFormValues = z.infer<typeof appointmentsFormSchema>;
@@ -29,9 +31,9 @@ export const AppointmentForm = () => {
     resolver: zodResolver(appointmentsFormSchema),
     defaultValues: {
       tutorName: '',
-      // petName: '',
-      // phone: '',
-      // description: '',
+      petName: '',
+      phone: '',
+      description: '',
     },
   });
 
@@ -61,20 +63,97 @@ export const AppointmentForm = () => {
           </DialogDescription>
         </DialogHeader>
 
-        <form onSubmit={form.handleSubmit(onSubmit, onError)}>
+        <form
+          onSubmit={form.handleSubmit(onSubmit, onError)}
+          className="space-y-4"
+        >
           <FieldGroup>
             <Controller
               name="tutorName"
               control={form.control}
               render={({ field, fieldState }) => (
                 <Field>
-                  <FieldLabel htmlFor={field.name}>Nome do Tutor</FieldLabel>
-                  <Input {...field} id={field.name} />
+                  <FieldLabel
+                    htmlFor={field.name}
+                    className="text-label-medium-size text-content-primary"
+                  >
+                    Nome do tutor
+                  </FieldLabel>
+                  <div className="relative">
+                    <User
+                      className="absolute left-3 top-1/2 -translate-y-1/2 transform text-content-brand"
+                      size={20}
+                    />
+                    <Input
+                      {...field}
+                      id={field.name}
+                      placeholder="Nome do tutor"
+                      className="pl-10"
+                    />
+                  </div>
+                  {fieldState.invalid && (
+                    <FieldError errors={[fieldState.error]} />
+                  )}
                 </Field>
               )}
             ></Controller>
-            <Button type="submit">Submit</Button>
+
+            <Controller
+              name="petName"
+              control={form.control}
+              render={({ field, fieldState }) => (
+                <Field>
+                  <FieldLabel
+                    htmlFor={field.name}
+                    className="text-label-medium-size text-content-primary"
+                  >
+                    Nome do pet
+                  </FieldLabel>
+                  <div className="relative">
+                    <Dog
+                      className="absolute left-3 top-1/2 -translate-y-1/2 transform text-content-brand"
+                      size={20}
+                    />
+                    <Input
+                      {...field}
+                      id={field.name}
+                      placeholder="Nome do pet"
+                      className="pl-10"
+                    />
+                  </div>
+                  {fieldState.invalid && (
+                    <FieldError errors={[fieldState.error]} />
+                  )}
+                </Field>
+              )}
+            ></Controller>
+
+            <Controller
+              name="description"
+              control={form.control}
+              render={({ field, fieldState }) => (
+                <Field>
+                  <FieldLabel
+                    htmlFor={field.name}
+                    className="text-label-medium-size text-content-primary"
+                  >
+                    Descrição do serviço
+                  </FieldLabel>
+
+                  <Textarea
+                    {...field}
+                    id={field.name}
+                    placeholder="Descrição do serviço"
+                    className="resize-none"
+                  />
+                  {fieldState.invalid && (
+                    <FieldError errors={[fieldState.error]} />
+                  )}
+                </Field>
+              )}
+            ></Controller>
           </FieldGroup>
+          <Button type="submit">Salvar</Button>
         </form>
       </DialogContent>
     </Dialog>
